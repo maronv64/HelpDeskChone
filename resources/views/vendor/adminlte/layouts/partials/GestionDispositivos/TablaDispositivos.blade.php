068cd390d91ee12c241f8bedb7c81f5b32c6212e
CesarIAndrade <cias_ec@hotmail.com>
2018-09-14 13:03:19 -0500

    d

--- resources/views/vendor/adminlte/layouts/partials/GestionDispositivos/TablaDispositivos.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/GestionDispositivos/TablaDispositivos.blade.php
@@ -1,33 +1,32 @@
-    <div class="row register-box-body">
-        <div class="row">
-           <div class="col-md-8">
-              <p> <h3>Lista de dispositivos registrados</h3></p>
-            </div>
-            <div class="col-md-4">
-                <a id="id_mostrar_dispositivos" class="btn btn-info  btn-block rounded" title="">Listar dispositivos</a>
-            </div>
-        </div>
-           
-          <hr>
-          <div class="table-responsive pre-scrollable">
-            <table id="tablaDispositivos" class="table table-hover table-bordered">
-            <thead>
-              <tr>
-                <th scope="col">Id dispositivo</th>
-                <th scope="col">Descripcion</th>
-                <th scope="col">Tipo de dispositivo</th>
-                <th scope="col">Serie</th>
-                <th scope="col">Color</th>
-                <th scope="col">Modelo</th>
-                <th scope="col">Marca</th>
-                <th scope="col">Estado</th>
-                <th scope="col">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-              </tr>
-            </tbody>
-          </table>
-          </div>       
+<div class="row register-box-body">
+  <div class="row">
+      <div class="col-md-8">
+        <p> <h3>Lista de dispositivos registrados</h3></p>
       </div>
+      <div class="col-md-4">
+          <a id="id_mostrar_dispositivos" class="btn btn-info  btn-block rounded" title="">Listar dispositivos</a>
+      </div>
+  </div> 
+  <hr>
+  <div class="table-responsive pre-scrollable">
+    <table id="tablaDispositivos" class="table table-hover table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">Id dispositivo</th>
+          <th scope="col">Descripcion</th>
+          <th scope="col">Tipo de dispositivo</th>
+          <th scope="col">Serie</th>
+          <th scope="col">Color</th>
+          <th scope="col">Modelo</th>
+          <th scope="col">Marca</th>
+          <th scope="col">Estado</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+        </tr>
+      </tbody>
+    </table>
+  </div>       
+</div>

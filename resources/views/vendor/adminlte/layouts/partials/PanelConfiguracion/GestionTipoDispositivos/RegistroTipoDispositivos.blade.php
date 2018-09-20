ba8b965489f304b7dc009aca2fe11fc318155da7
luiscoaboy <luiscoaboyi@gmail.com>
2018-09-20 16:51:00 -0500

    Avances con el panel 1.2

--- resources/views/vendor/adminlte/layouts/partials/PanelConfiguracion/GestionTipoDispositivos/RegistroTipoDispositivos.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/PanelConfiguracion/GestionTipoDispositivos/RegistroTipoDispositivos.blade.php
@@ -1,21 +1,21 @@
-                  <form id="frm_ingresar_tipos_user"  method="post">
+                  <form id="frm_ingresar_tipos_dispositivos"  method="post">
                 
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <div class="row">
                         <div class="col-md-8">
-                            <img src="img/imagenes/Cuentasusuarios.png" alt="" class="img-responsive img-rounded">
+                            <img src="img/imagenes/dispositivos.jpg" alt="" class="img-responsive img-rounded">
                         </div>
                         <div class="col-md-4">
                             <div class="form-group has-feedback">
                                 <div>
-                                <center><h3>Ingreso de nuevos tipos de usuarios</h3></center>  
+                                <center><h3>Ingreso de nuevos tipos de dispositivos</h3></center>  
                                 </div>
                                 <br>
-                                <input required id="" type="text" class="form-control" placeholder="Descripcion del tipo de usuario" autocomplete="off" value="{{ old('dispositivo') }}" />
+                                <input required id="" type="text" class="form-control" placeholder="Descripcion del tipo de usuario" autocomplete="off" value="{{ old('') }}" />
                                 <br>
                                 <button type="submit"  class="btn btn-primary btn-block rounded">Almacenar</button>
                             </div>
                         </div>
                     </div>                    
                   </form>
-                  @include('adminlte::layouts.partials.PanelConfiguracion.GestionTipoUsuarios.Tabla')  
+                  @include('adminlte::layouts.partials.PanelConfiguracion.GestionTipoDispositivos.TablaDispositivos')  
--- resources/views/vendor/adminlte/layouts/partials/PanelConfiguracion/GestionTipoDispositivos/TablaDispositivos.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/PanelConfiguracion/GestionTipoDispositivos/TablaDispositivos.blade.php
@@ -1,19 +1,16 @@
 <div class="row register-box-body">
         <div class="row">
            <div class="col-md-8">
-              <p> <h3>Lista de areas registradas</h3></p>
+              <p> <h3>Lista de tipos de dispositivos registrados</h3></p>
             </div>
         </div>
            
           <hr>
           <div class="table-responsive pre-scrollable">
-            <table id="tabla_Areas" class="table table-hover table-bordered">
+            <table id="tablaTiposDispositivos" class="table table-hover table-bordered">
             <thead>
               <tr>
-                <th scope="col">Nombre del area</th>
-                <th scope="col">Correo</th>
-                <th scope="col">Extensión</th>
-                <th scope="col">Siglas</th>
+                <th scope="col">Descripcion</th>
                 <th scope="col">Acciones</th>
               </tr>
             </thead>

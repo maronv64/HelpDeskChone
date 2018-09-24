5479ecb449256c2ecc69b3532205e6e9ca2ff362
luiscoaboy <luiscoaboyi@gmail.com>
2018-09-24 09:52:55 -0500

    conexion

--- public/js/PanelConfiguracion/GestionPanelEspecialidad.js
+++ public/js/PanelConfiguracion/GestionPanelEspecialidad.js
@@ -2,10 +2,10 @@ $('#id_toggle_especialidades').click(function(event)
 	{
 		MostrarExtraTecnicos();
 });
-$('#frm_ingresar_tipos_dispositivos').on('submit',function(e){
-	e.preventDefault();
-	guardarTipoDispositivo();
-})
+// $('#frm_ingresar_tipos_dispositivos').on('submit',function(e){
+// 	e.preventDefault();
+// 	guardarTipoEspecialidades();
+// })
 
 function MostrarExtraTecnicos(){
     $('#tablaEspecialidades tbody tr').empty();
@@ -20,29 +20,29 @@ function MostrarExtraTecnicos(){
             }); 
     });
 }
-function guardarTipoDispositivo() {
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    }); 
-    var FrmData = {
-        descripcion:$('#id_txt_tipo_dispositivo').val(),
-    }
-    $.ajax({
-        url:'tiposdispositivos', // Url que se envia para la solicitud
-        method: 'POST',             // Tipo de solicitud que se enviará, llamado como método
-        data: FrmData,               // Datos enviados al servidor, un conjunto de pares clave / valor (es decir, campos de formulario y valores)
-        dataType: 'json',
-        success: function(requestData)   // Una función a ser llamada si la solicitud tiene éxito
-        {
-            alertify.success("DATOS INGRESADOS CORRECTAMENTE");
-            MostrarTiposDispositivos();
-            $('#id_txt_tipo_dispositivo').val('')
-        },
-        fail:function(){
-            alertify.error("HA OCURRIDO UN ERROR");
-            $('#id_txt_tipo_dispositivo').val('')
-        }
-    });
-}
+// function guardarTipoEspecialidades() {
+//     $.ajaxSetup({
+//         headers: {
+//             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+//         }
+//     }); 
+//     var FrmData = {
+//         descripcion:$('#id_txt_tipo_dispositivo').val(),
+//     }
+//     $.ajax({
+//         url:'tiposdispositivos', // Url que se envia para la solicitud
+//         method: 'POST',             // Tipo de solicitud que se enviará, llamado como método
+//         data: FrmData,               // Datos enviados al servidor, un conjunto de pares clave / valor (es decir, campos de formulario y valores)
+//         dataType: 'json',
+//         success: function(requestData)   // Una función a ser llamada si la solicitud tiene éxito
+//         {
+//             alertify.success("DATOS INGRESADOS CORRECTAMENTE");
+//             MostrarTiposDispositivos();
+//             $('#id_txt_tipo_dispositivo').val('')
+//         },
+//         fail:function(){
+//             alertify.error("HA OCURRIDO UN ERROR");
+//             $('#id_txt_tipo_dispositivo').val('')
+//         }
+//     });
+// }
